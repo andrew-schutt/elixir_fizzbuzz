b64befac0afe97f4d2147e997021bd2235412b2b
Andrew Schutt <schutt.andrew@gmail.com>
2017-01-18 23:32:32 -0600

    added case version of fizzbuzz

---
+++ fizzbuzz4.ex
@@ -0,0 +1,19 @@
+defmodule FizzBuzz do
+
+  def upto(n) when n > 0 do
+    1..n |> Enum.map(&fizzbuzz/1)
+  end
+
+  def fizzbuzz(n) do
+    case n do
+      n when rem(n, 3) == 0 and rem(n, 5) == 0 ->
+        "FizzBuzz"
+      n when rem(n, 3) == 0 ->
+        "Fizz"
+      n when rem(n, 5) == 0 ->
+        "Buzz"
+      _ ->
+        n
+    end
+  end
+end
